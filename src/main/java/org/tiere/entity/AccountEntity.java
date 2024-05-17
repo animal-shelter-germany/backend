10bbe497c6ca34cbdc0d31f5e3a8d1783a2cc5cb
Andreas Dinauer <andreas.j.dinauer@gmail.com>
2024-05-17 16:28:24 +0200

    first and last name added to account

--- src/main/java/org/tiere/entity/AccountEntity.java
+++ src/main/java/org/tiere/entity/AccountEntity.java
@@ -16,6 +16,12 @@ public class AccountEntity extends PanacheEntityBase {
 
     private String email;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     @Column(name = "account_password")
     private String password;
 
@@ -28,6 +34,22 @@ public class AccountEntity extends PanacheEntityBase {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getPassword() {
